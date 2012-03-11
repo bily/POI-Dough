c1f5c941db669e977e758b881c43baa3f7d17939
Nick Doiron <nickd@codeforamerica.org>
2012-03-10 16:07:23 -0800

    moving OSM XML to JSON to Heroku

--- lib/pdf_archive.rb
+++ lib/pdf_archive.rb
@@ -80,7 +80,6 @@ get '/isometrics/:wayid' do
     	res = Net::HTTP.start(url.host, url.port) {|http|
     	  http.get('/api/0.6/way/' + wayid + '/full')
     	}
-    	return res.body
     	
 		gotdata = res.body.split("\n")
 		
